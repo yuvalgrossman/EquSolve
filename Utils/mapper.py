ceb9af7b0f799d8031284709ad929d51519592ea
shaharHegyi <55831788+shaharHegyi@users.noreply.github.com>
2020-08-18 17:04:22 +0300

    Create mapper.py

---
+++ Utils/mapper.py
@@ -0,0 +1,11 @@
+def mapper(csv_df,sym_list=None):
+  # selects slice of data frame according to list of symbols
+  # sym_list is a list of latex symbols (e.g. ['1','A','\pi'])
+  if sym_list == None:
+    return csv_df
+  else:
+    df = csv_df[['latex','symbol_id']][csv_df['latex'].isin(sym_list)].reset_index(drop=True)
+    latex_id_df = df.drop_duplicates().reset_index(drop=True)
+    label_map = dict(zip(latex_id_df['symbol_id'], list(latex_id_df.index)))
+    df['symbol_id'].replace(label_map, inplace=True)
+    return df
