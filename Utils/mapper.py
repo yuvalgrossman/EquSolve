2a6a5123a79f2ad0b76edf1e8cbd7b19e1065630
shaharHegyi <55831788+shaharHegyi@users.noreply.github.com>
2020-08-25 10:29:33 +0300

    Update mapper.py

--- Utils/mapper.py
+++ Utils/mapper.py
@@ -6,7 +6,7 @@ def mapper(csv_df,sym_list=None):
   if sym_list == None:
     return csv_df
   else:
-    df = csv_df[['Path','latex','symbol_id']][csv_df['latex'].isin(sym_list)].reset_index(drop=True)
+    df = csv_df[['path','latex','symbol_id']][csv_df['latex'].isin(sym_list)].reset_index(drop=True)
     latex_id_df = df.drop_duplicates().reset_index(drop=True)
     label_map = dict(zip(latex_id_df['symbol_id'], list(latex_id_df.index)))
     df['symbol_id'].replace(label_map, inplace=True)
