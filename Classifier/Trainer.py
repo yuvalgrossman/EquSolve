798865b31fbb1d79269193a792eae4184f11cc5c
yuvalgrossman <gryuval@gmail.com>
2020-08-27 10:47:14 +0300

    add export measures

--- Classifier/Trainer.py
+++ Classifier/Trainer.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+import datetime
+import os
 
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
@@ -24,19 +26,24 @@ class Trainer():
         self.config = config
         self.device = self.get_device()
 
+        #create dir to save train results:
+        theTime = "{date:%Y-%m-%d_%H-%M-%S}".format(date=datetime.datetime.now())
+        self.Train_Results_Dir = 'TrainResults/Train_Results_' + theTime
+        os.mkdir(self.Train_Results_Dir)
+
     def train_classifier_HASY(self):
         # DATA LOADER:
 
         # if 'hasy-data-labels.csv'
 
-        train_df = pd.read_csv(self.config['data_path'] + self.config['train_data_path'])
-        test_df = pd.read_csv(self.config['data_path'] + self.config['test_data_path'])
+        train_df = pd.read_csv(self.config['data_path'] + self.config['train_data_path'])[:1024]
+        test_df = pd.read_csv(self.config['data_path'] + self.config['test_data_path'])[:1024]
 
         train_data = ExampleDataset(self.config, train_df, transforms=transforms.ToTensor())
         trainloader = DataLoader(train_data, batch_size=self.config['batch_size'], shuffle=True)
 
         test_data = ExampleDataset(self.config, test_df, transforms=transforms.ToTensor())
-        testloader = DataLoader(train_data, batch_size=self.config['batch_size'], shuffle=True)
+        testloader = DataLoader(test_data, batch_size=self.config['batch_size'], shuffle=True)
 
         # TRAINING CONFIGURATIONS:
         net = SimpleClassifier().to(self.device)
@@ -44,56 +51,105 @@ class Trainer():
         # loss
         self.criterion = nn.CrossEntropyLoss()
         # optimizer
-        self.optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        self.optimizer = optim.SGD(net.parameters(), lr=self.config['lr'], momentum=0.9)
 
-        net = self.train(net, trainloader)
+        #define tracking measures:
+        self.tracking_measures = {}
+        self.tracking_measures['batch_train_loss'] = []
+        self.tracking_measures['batch_train_acc'] = []
+        self.tracking_measures['epoch_train_loss'] = []
+        self.tracking_measures['epoch_train_acc'] = []
+        self.tracking_measures['epoch_test_loss'] = []
+        self.tracking_measures['epoch_test_acc'] = []
+
+        print('Start Training on {}'.format(self.device))
+
+        for epochNum in range(self.config['train_epochs']):  # no. of epochs
+
+            net = self.train_epoch(net, trainloader, epochNum)
+
+            self.test_epoch(net, testloader, epochNum)
 
         self.save_network(net)
 
-        self.test(net, testloader)
+        print('Done Training {} epochs'.format(epochNum+1))
 
+        self.generate_measures_plots()
 
-    def train(self, net, trainloader):
-        print('Start Training on {}'.format(self.device))
+    def generate_measures_plots(self):
+        for key, value in self.tracking_measures.items():
+            fig, ax = plt.subplots(figsize=(10, 5))
+            ax.plot(value)
+            ax.set_title(key)
+            plt.grid()
+            fn = os.path.join(self.Train_Results_Dir,'{}.png'.format(key))
+            plt.savefig(fn)
 
-        for epoch in range(self.config['train_epochs']):  # no. of epochs
-            running_loss = 0
-            for data in tqdm(trainloader):
-                # data pixels and labels to GPU if available
-                inputs, labels = data[0].to(self.device, non_blocking=True), data[1].to(self.device, non_blocking=True)
-                # print(inputs.shape, labels.shape)
-                # set the parameter gradients to zero
-                self.optimizer.zero_grad()
-                outputs = net(inputs)
-                # print(outputs.shape, labels.shape)
-                loss = self.criterion(outputs, labels)
-                # propagate the loss backward
-                loss.backward()
-                # update the gradients
-                self.optimizer.step()
+    def train_epoch(self, net, trainloader, epoch):
 
-                running_loss += loss.item()
-            print('[Epoch %d] loss: %.3f' %
-                  (epoch + 1, running_loss / len(trainloader)))
+        epoch_loss = 0
+        epoch_acc = 0
+        for data in tqdm(trainloader):
+            # data pixels and labels to GPU if available
+            inputs, labels = data[0].to(self.device, non_blocking=True), data[1].to(self.device, non_blocking=True)
+            # print(inputs.shape, labels.shape)
+            # set the parameter gradients to zero
+            self.optimizer.zero_grad()
+            outputs = net(inputs)
+            # print(outputs.shape, labels.shape)
+            loss = self.criterion(outputs, labels)
+            # propagate the loss backward
+            loss.backward()
+            # update the gradients
+            self.optimizer.step()
 
-        print('Done Training')
+            batch_loss = loss.item()
+            epoch_loss += batch_loss
+            self.tracking_measures['batch_train_loss'].append(batch_loss)
+
+            _, predicted = torch.max(outputs.data, 1)
+            batch_acc = (predicted == labels).sum().item()
+            epoch_acc += batch_acc
+            self.tracking_measures['batch_train_acc'].append(batch_acc)
+
+        epoch_loss /= len(trainloader)
+        epoch_acc /= len(trainloader)
+        print('Train Epoch {} loss: {:.3f} acc: {:.3f}'.format(epoch + 1, epoch_loss, epoch_acc))
+        self.tracking_measures['epoch_train_loss'].append(epoch_loss)
+        self.tracking_measures['epoch_train_acc'].append(epoch_acc)
+
         return net
 
 
-    def test(self, net, testloader):
+    def test_epoch(self, net, testloader, epoch):
         correct = 0
         total = 0
+        epoch_loss = 0
+        epoch_acc = 0
         with torch.no_grad():
             for data in testloader:
                 inputs, labels = data[0].to(self.device, non_blocking=True), data[1].to(self.device, non_blocking=True)
                 # print(inputs.shape, labels.shape)
                 outputs = net(inputs)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                loss = self.criterion(outputs, labels)
 
-        print('Accuracy of the network on test images: %0.3f %%' % (
-                100 * correct / total))
+                # total += labels.size(0)
+                # correct += (predicted == labels).sum().item()
+
+                batch_loss = loss.item()
+                epoch_loss += batch_loss
+
+                _, predicted = torch.max(outputs.data, 1)
+                batch_acc = (predicted == labels).sum().item()
+                epoch_acc += batch_acc
+
+        # print('Accuracy of the network on test images: %0.3f %%' % (
+        #         100 * correct / total))
+        epoch_loss /= len(testloader)
+        epoch_acc /= len(testloader)
+        print('Test Epoch {} loss: {:.3f} acc: {:.3f}'.format(epoch + 1, epoch_loss, epoch_acc))
+        self.tracking_measures['epoch_test_loss'].append(epoch_loss)
+        self.tracking_measures['epoch_test_acc'].append(epoch_acc)
 
     def get_device(self):
         if torch.cuda.is_available():
@@ -103,8 +159,11 @@ class Trainer():
         return device
 
     def save_network(self, net):
-        PATH = 'Weights/HASY_simpleclassifier.pth'
-        torch.save(net.state_dict(), PATH)
+        fn = os.path.join(self.Train_Results_Dir, 'HASY_simpleclassifier.pth')
+        saved_dict = net.state_dict()
+        saved_dict['tracking_measures'] = self.tracking_measures
+        saved_dict['config'] = self.config
+        torch.save(net.state_dict(), fn)
 
 #     device = get_device()
 # train(net, trainloader)
--- Classifier/main.py
+++ Classifier/main.py
@@ -2,23 +2,27 @@ from Classifier.Trainer import Trainer
 import tarfile
 import pandas as pd
 import os
+inner_path = '/home/yuval/Projects/'
 
 def download_dataset():
     # os.system('wget -P DataSets/HASY https://zenodo.org/record/259444/files/HASYv2.tar.bz2?download=1')
     # my_tar = tarfile.open('HASYv2.tar.bz2?download=1')
     # my_tar.extractall()  # specify which folder to extract to
     # my_tar.close()
-    meta_data = pd.read_csv('/home/yuval/Projects/EquSolve/DataSets/HASY/hasy-data-labels.csv')
+    meta_data = pd.read_csv(inner_path + 'EquSolve/DataSets/HASY/hasy-data-labels.csv')
     return meta_data
 
 
-meta_data = download_dataset()
+if not os.path.exists(inner_path + 'EquSolve/DataSets/HASY/hasy-data'):
+    meta_data = download_dataset()
 config = {}
-config['data_path'] = '/home/yuval/Projects/EquSolve/DataSets/HASY/'
+config['inner_path'] = inner_path
+config['data_path'] = inner_path + 'EquSolve/DataSets/HASY/'
 config['train_data_path'] = 'classification-task/fold-1/train.csv'
 config['test_data_path']  = 'classification-task/fold-1/test.csv'
-config['batch_size'] = 128
-config['train_epochs'] = 1
+config['batch_size'] = 32
+config['train_epochs'] = 10
+config['lr'] = 0.001
 
 theTrainer = Trainer(config)
 theTrainer.train_classifier_HASY()
