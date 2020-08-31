e55acfc15db480ff7f5e6d30cfbd90ace8fdecba
shaharHegyi <55831788+shaharHegyi@users.noreply.github.com>
2020-08-31 14:58:11 +0300

    Update SimpleClassifier.py

--- Classifier/SimpleClassifier.py
+++ Classifier/SimpleClassifier.py
@@ -4,23 +4,22 @@ import torch.nn.functional as F
 # define the neural net class
 class SimpleClassifier(nn.Module):
     def __init__(self):
-        super(SimpleClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20,
-                               kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50,
-                               kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(in_features=1800, out_features=500)
-        self.fc2 = nn.Linear(in_features=500, out_features=1401)
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         # print(x.shape)
-        x = F.relu(self.conv1(x));  # print(x.shape)
-        x = F.max_pool2d(x, 2, 2);  # print(x.shape)
-        x = F.relu(self.conv2(x));  # print(x.shape)
-        x = F.max_pool2d(x, 2, 2);  # print(x.shape)
-        x = x.view(x.size(0), -1);  # print(x.shape)
-        x = F.relu(self.fc1(x));  # print(x.shape)
-        x = self.fc2(x);  # print(x.shape)
+        x = self.pool(F.relu(self.conv1(x)));
+        x = self.pool(F.relu(self.conv2(x)));
+        x = x.view(-1, 16 * 4 * 4); 
+        x = F.relu(self.fc1(x)); 
+        x = F.relu(self.fc2(x));
+        x = self.fc3(x);
         return x
 
 if __name__ == "__main__":
