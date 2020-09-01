ac3cd2d7eb6c4123297461e36d39d66f2ddc3874
shaharHegyi <55831788+shaharHegyi@users.noreply.github.com>
2020-09-01 16:29:24 +0300

    Update mapper.py

--- Utils/mapper.py
+++ Utils/mapper.py
@@ -8,7 +8,7 @@ def mapper(csv_df,sym_list=None):
   else:
     df = csv_df[['path','latex','symbol_id']][csv_df['latex'].isin(sym_list)].reset_index(drop=True) # slice rows according to sym_list
     latex_id_df = df.drop_duplicates().reset_index(drop=True)
-    old_map = {df.latex[i]:df.symbol_id[i] for i in range(len(sym_list))}      # create dictionary for old symbol_id
-    label_map = {old_map[sym_list[i]]:i for i in range(len(sym_list))}         # create dictionary for new symbol_id
+    old_map = df.groupby('latex')['symbol_id'].unique().astype('int').to_dict()
+    label_map = {old_map.get(sym_list[i]):sym_list.index(sym_list[i]) for i in range(len(old_map))}
     df['symbol_id'].replace(label_map, inplace=True)
     return df
