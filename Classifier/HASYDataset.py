ca8cd379d59b0d77531175666d8984d8df224342
shaharHegyi <55831788+shaharHegyi@users.noreply.github.com>
2020-09-01 11:22:14 +0300

    Update HASYDataset.py

--- Classifier/HASYDataset.py
+++ Classifier/HASYDataset.py
@@ -2,15 +2,11 @@ from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from Utils.mapper import mapper
-import tarfile
-import pandas as pd
-import os
 
 class HASYDataset(Dataset):
-    def __init__(self, config, transforms=None):
+    def __init__(self, config, csv_df,transforms):
         self.config = config
-        self.prepare_hasy_dataset()
+        self.data = csv_df
         self.transforms = transforms
 
     def __len__(self):
@@ -18,15 +14,11 @@ class HASYDataset(Dataset):
 
     def __getitem__(self, idx):
         y = self.data.loc[idx, 'symbol_id']
-        img = self.read_image(idx)
+        img_path = self.config['data_path'] + self.data.loc[idx, 'path']
+        img = Image.open(img_path) # load image in PIL format
         X = self.transforms(img)   # apply transforms: resize-> tensor-> normalize
         X_reshape = X[0].unsqueeze(-1).transpose(2,0) # reshape to [1,28,28]
         return (X_reshape, y)
-
-    def read_image(self, idx):
-        img_path = self.config['data_path'] + self.data.loc[idx, 'path']
-        return Image.open(img_path) # load image in PIL format
-
 
     def plotitem(self, idx):
         y = self.data.loc[idx, 'latex']
@@ -35,18 +27,3 @@ class HASYDataset(Dataset):
         plt.imshow(X)
         plt.title(y)
         print('img size {}'.format(X.shape))
-
-    def prepare_hasy_dataset(self):
-        if not os.path.exists(self.config['dataset_path']):
-            os.system('wget -P DataSets/HASY https://zenodo.org/record/259444/files/HASYv2.tar.bz2?download=1')
-            my_tar = tarfile.open('HASYv2.tar.bz2?download=1')
-            my_tar.extractall()  # specify which folder to extract to
-            my_tar.close()
-        full_df = pd.read_csv(self.config['dataset_path'])
-
-        sym_list = None if 'sym_list' not in self.config.keys() else self.config['sym_list']
-
-        self.data = mapper(full_df, sym_list)
-
-        self.class2sym_mapper = np.array(self.data.groupby('symbol_id').first()['latex'])
-
